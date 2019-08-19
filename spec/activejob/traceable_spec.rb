a141139130891bc027a40372a064553bc9d061cc
Oksana Dushenkivska <oksana.dushenkivska@qonto.eu>
2019-08-19 14:10:50 +0200

    Fix setting and getting tracing_info

--- spec/activejob/traceable_spec.rb
+++ spec/activejob/traceable_spec.rb
@@ -9,17 +9,49 @@ RSpec.describe 'ActiveJobTraceableJob', type: :job do
 
   class ActiveJobTraceableJob < ActiveJob::Base; end
 
-  before do
-    ActiveJob::Traceable.tracing_info_getter = lambda do
-      {
-        first_attribute: CurrentScope.first_attribute,
-        second_attribute: CurrentScope.second_attribute
-      }
+  describe '.tracing_info_getter=' do
+    context 'when it does not respond to :call' do
+      it 'raises an error' do
+        expect {
+          ActiveJob::Traceable.tracing_info_getter = 'getter'
+        }.to raise_error('Tracing info getter should be callable')
+      end
     end
 
-    ActiveJob::Traceable.tracing_info_setter = lambda do |attributes|
-      CurrentScope.first_attribute = attributes[:first_attribute]
-      CurrentScope.second_attribute = attributes[:second_attribute]
+    context 'when it does not contain a hash' do
+      it 'raises an error' do
+        expect {
+          ActiveJob::Traceable.tracing_info_getter = -> { 'getter' }
+        }.to raise_error('Tracing info getter should contain a hash')
+      end
+    end
+
+    context 'when it is callable and contains a hash' do
+      it 'assigns the variable' do
+        value = -> { { attribute: 'getter' } }
+        expect {
+          ActiveJob::Traceable.tracing_info_getter = value
+        }.to change(ActiveJob::Traceable, :tracing_info_getter).to(value)
+      end
+    end
+  end
+
+  describe '.tracing_info_setter=' do
+    context 'when it does not respond to :call' do
+      it 'raises an error' do
+        expect {
+          ActiveJob::Traceable.tracing_info_setter = 'setter'
+        }.to raise_error('Tracing info setter should be callable')
+      end
+    end
+
+    context 'when it is callable' do
+      it 'assigns the variable' do
+        value = -> { { attribute: 'setter' } }
+        expect {
+          ActiveJob::Traceable.tracing_info_setter = value
+        }.to change(ActiveJob::Traceable, :tracing_info_setter).to(value)
+      end
     end
   end
 
@@ -27,6 +59,18 @@ RSpec.describe 'ActiveJobTraceableJob', type: :job do
     subject(:job) { ActiveJobTraceableJob.perform_later }
 
     before do
+      ActiveJob::Traceable.tracing_info_getter = lambda do
+        {
+          first_attribute: CurrentScope.first_attribute,
+          second_attribute: CurrentScope.second_attribute
+        }
+      end
+
+      ActiveJob::Traceable.tracing_info_setter = lambda do |attributes|
+        CurrentScope.first_attribute = attributes[:first_attribute]
+        CurrentScope.second_attribute = attributes[:second_attribute]
+      end
+
       CurrentScope.first_attribute = 'first_attribute_value'
       CurrentScope.second_attribute = 'second_attribute_value'
     end
@@ -37,6 +81,13 @@ RSpec.describe 'ActiveJobTraceableJob', type: :job do
           first_attribute: 'first_attribute_value',
           second_attribute: 'second_attribute_value',
         )
+      end
+
+      context 'when tracing_info_getter is not set' do
+        it 'contains an empty hash' do
+          ActiveJob::Traceable.tracing_info_getter = nil
+          expect(job.tracing_info).to eq({})
+        end
       end
     end
 
@@ -110,7 +161,7 @@ RSpec.describe 'ActiveJobTraceableJob', type: :job do
       ActiveJobTraceableJob.perform_later
 
       expect(logger.messages).to include(
-        '{:first_attribute=>"first_attribute_value", :second_attribute=>"second_attribute_value"}',
+        "first_attribute_value", "second_attribute_value"
       )
     end
   end
